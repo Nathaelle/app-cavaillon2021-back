7126684912d23a3456fce0b3af7a8de1a6b7e160
Nathaelle <scriptales.clients@gmail.com>
2021-09-03 10:26:34 +0200

    Export des données d'une catégorie

--- src/main/java/com/ecommerce/CategoryController.java
+++ src/main/java/com/ecommerce/CategoryController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.entities.Category;
+import com.ecommerce.entities.Item;
 import com.ecommerce.repositories.CategoryRepository;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -21,6 +25,24 @@ public class CategoryController {
 	public List<Category> getAll() {
 		
 		List<Category> cats = this.catRepo.findAll();
+		for(Category cat: cats) {
+			for(Item it: cat.getItems()) {
+				it.setCategory(null);
+			}
+		}
+		
 		return cats;
+	}
+	
+	@GetMapping("/categories-{id}")
+	public Category showCategory(@PathVariable int id) {
+		
+		Category category = (catRepo.existsById(id))? catRepo.getById(id) : new Category();
+		for(Item it: category.getItems()) {
+			it.setCategory(null);
+		}
+		
+		return category;
+		
 	}
 }
--- src/main/java/com/ecommerce/entities/Category.java
+++ src/main/java/com/ecommerce/entities/Category.java
@@ -1,11 +1,16 @@
 package com.ecommerce.entities;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -25,9 +30,9 @@ public class Category {
 	private String shortText;
 	
 	// Une entité Category est en relation avec plusieurs entités Item
-//	@OneToMany
-//	@JoinColumn(name="category_id", referencedColumnName="id")
-//	private List<Item> items = new ArrayList<Item>();
+	@OneToMany
+	@JoinColumn(name="category_id", referencedColumnName="id")
+	private List<Item> items = new ArrayList<Item>();
 	
 
 	public int getId() {
@@ -54,13 +59,13 @@ public class Category {
 		this.shortText = shortText;
 	}
 
-//	public List<Item> getItems() {
-//		return items;
-//	}
-//
-//	public void setItems(List<Item> items) {
-//		this.items = items;
-//	}
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
 
 	@Override
 	public String toString() {
