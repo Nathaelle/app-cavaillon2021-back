fe1e50e2e777b8ac64b1739f1ef131c5f0bb718f
Nathaelle <scriptales.clients@gmail.com>
2021-09-03 11:53:22 +0200

    Ajout d'une route pour ajout catégorie

--- src/main/java/com/ecommerce/CategoryController.java
+++ src/main/java/com/ecommerce/CategoryController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.entities.Category;
@@ -43,6 +42,11 @@ public class CategoryController {
 		}
 		
 		return category;
+		
+	}
+	
+	@PostMapping("/add-category")
+	public void addCategory() {
 		
 	}
 }
